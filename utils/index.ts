96c878830c57a788f7e2a4aaf052b80d8cf41464
adam ye macbook <adamye001@gmail.com>
2023-06-11 21:16:44 +1000

    Add Support for Car Image API

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
     const headers = {
         "X-RapidAPI-Key": "014a6c665fmshab859266b4409fbp1cf5dfjsn181a6f7ce500",
         "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
     };
     const response = await fetch(
-        "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera",
+        "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=cayenne",
         { headers: headers }
     );
 
@@ -26,4 +28,19 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
     return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+
+    const { make, year, model } = car;
+
+    url.searchParams.append("customer", "auadamye");
+    url.searchParams.append("make", make);
+    url.searchParams.append("modelFamily", model.split(" ")[0]);
+    url.searchParams.append("zoomType", "fullscreen");
+    url.searchParams.append("modelYear", `${year}`);
+    url.searchParams.append("angle", `${angle}`);
+
+    return `${url}`;
 };
