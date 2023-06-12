42a26cfcfd4159b4835fa94b1bc60c9f92464153
adam ye macbook <adamye001@gmail.com>
2023-06-12 21:15:16 +1000

    Add support for client version

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -6,8 +6,8 @@ import { manufacturers } from "@/constants";
 import { SearchManufacturerProps } from "@/types";
 
 const SearchManufacturer = ({
-    manufacturer,
-    setManufacturer,
+    selected,
+    setSelected,
 }: SearchManufacturerProps) => {
     const [query, setQuery] = useState("");
 
@@ -23,7 +23,7 @@ const SearchManufacturer = ({
 
     return (
         <div className="search-manufacturer">
-            <Combobox value={manufacturer} onChange={setManufacturer}>
+            <Combobox value={selected} onChange={setSelected}>
                 <div className="relative w-full">
                     <Combobox.Button className="absolute top-[14px]">
                         <Image
@@ -48,7 +48,10 @@ const SearchManufacturer = ({
                         leaveTo="opacity-0"
                         afterLeave={() => setQuery("")}
                     >
-                        <Combobox.Options>
+                        <Combobox.Options
+                            className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                            static
+                        >
                             {filteredManufacturers.map((item) => (
                                 <Combobox.Option
                                     key={item}
